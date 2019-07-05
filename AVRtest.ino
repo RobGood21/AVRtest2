ac4955c8a97201cdb2c02156757158072910239f
Rob Antonisse <robantonisse@gmail.com>
2019-07-05 23:09:25 +0200

    Add project files.

---
+++ AVRtest.ino
@@ -0,0 +1,21 @@
+/*
+    Name:       AVRtest.ino
+    Created:	5-7-2019 23:01:40
+    Author:     DESKTOP-UUF0FDC\gebruiker
+*/
+
+
+void setup()
+{
+	pinMode(0, OUTPUT);
+
+}
+
+void loop()
+{
+	digitalWrite(0, HIGH);
+	delay(50);
+	digitalWrite(0, LOW);
+	delay(100);
+
+}
