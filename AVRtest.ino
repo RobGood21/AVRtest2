02ebe848a690c7eb4c1e054092f8c6f7346813b4
Rob Antonisse <robantonisse@gmail.com>
2019-07-06 21:54:54 +0200

    AVRtest

--- AVRtest.ino
+++ AVRtest.ino
@@ -4,18 +4,36 @@
     Author:     DESKTOP-UUF0FDC\gebruiker
 */
 
+byte count;
+
 
 void setup()
 {
-	pinMode(0, OUTPUT);
-
+	DDRB |= (1 << 3);
+	//pinMode(0, OUTPUT);
+	TCCR1 |= (1 << 15);
+	//TCCR1 |= (1 << CS11);
+	//TCCR1 |= (1 << CS12);
+	TIMSK |= (1 << 2);
 }
 
+ISR(TIMER1_OVF_vect) {
+	count++;
+	if(count==0x0) PINB |= (1 << 3);
+}
 void loop()
 {
+
+	//PINB |= (1 << 3);
+	//delay(500);
+
+
+	/*
+	
 	digitalWrite(0, HIGH);
-	delay(50);
+	delay(500);
 	digitalWrite(0, LOW);
 	delay(100);
+*/
 
 }
