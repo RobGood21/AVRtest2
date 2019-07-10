8d3f2f64ba238a70b1cb810fb989394b136fc739
Rob Antonisse <robantonisse@gmail.com>
2019-07-10 22:34:36 +0200

    AVRtest

--- AVRtest.ino
+++ AVRtest.ino
@@ -29,7 +29,7 @@ byte DEK_Buf5[12];
 
 byte COM_reg;
 volatile unsigned int count = 0;
-byte shiftbyte = B11111000;
+byte shiftbyte;
 byte switchcount;
 byte switchstatus;
 unsigned long Ctime;
@@ -69,6 +69,7 @@ void setup()
 
 }
 
+/*
 void debug(byte type) {
 
 	switch (type) {
@@ -82,6 +83,7 @@ void debug(byte type) {
 	}
 
 }
+*/
 
 //deKoder Attiny85 begin ********************************************************
 ISR(PCINT0_vect) { //syntax voor een ISR
@@ -184,7 +186,7 @@ void DEK_BitRX() { //new version
 	case 0: //Waiting for preample 
 		if (bitRead(DEK_Reg, 3) == true) {
 			DEK_countPA++;
-			
+
 			if (DEK_countPA > 12) {
 				DEK_Status = 1;
 				countbit = 0;
@@ -195,7 +197,7 @@ void DEK_BitRX() { //new version
 		break;
 		//*************************
 	case 1: //Waiting for false startbit
-		
+
 		if (bitRead(DEK_Reg, 3) == false) { //startbit receive
 			DEK_countPA = 0;
 			DEK_Status = 2;
@@ -312,7 +314,7 @@ void APP_exe(boolean type, int adres, int decoder, int channel, boolean port, bo
 
 
 		if (port == true) {
-			shiftbyte |= (1 <<3);
+			shiftbyte |= (1 << 3);
 			//PORTB |= (1 << 1);
 		}
 		else {
@@ -331,50 +333,68 @@ ISR(TIMER1_OVF_vect) {
 
 }
 */
-void STEP() {
+void steps() {
 	byte temp;
 	switch (stepfase) {
-	case 0:
-		temp = B10010000;
+	case 0: //0010
+		shiftbyte |= (2 << 4);
+		shiftbyte &= ~(13 << 4);
 		break;
-	case 1:
-		temp = B01100000;
+	case 1: //0011
+		shiftbyte |= (3 << 4);
+		shiftbyte &= ~(12 << 4);
 		break;
-	case 2:
-		temp = B01100000;
+	case 2: //0001
+		shiftbyte |= (1 << 4);
+		shiftbyte &= ~(14 << 4);
 		break;
-	case 3:
-		temp = B10010000;
+	case 3: //1001
+		shiftbyte |= (9 << 4);
+		shiftbyte &= ~(6 << 4);
+		break;
+	case 4: //1000
+		shiftbyte |= (8 << 4);
+		shiftbyte &= ~(7 << 4);
+		break;
+	case 5: //1100
+		shiftbyte |= (12 << 4);
+		shiftbyte &= ~(3 << 4);
+		break;
+	case 6: //0100
+		shiftbyte |= (4 << 4);
+		shiftbyte &= ~(11 << 4);		
+		break;
+	case 7: //0110
+		shiftbyte |= (6 << 4);
+		shiftbyte &= ~(9 << 4);
 		break;
 	}
-	stepfase++;
-	if (stepfase > 3)stepfase = 0;
-
-	shiftbyte = shiftbyte << 5;
-	shiftbyte = shiftbyte >> 5; //clear bits 4~7
-	shiftbyte = shiftbyte + temp;
+	if (bitRead(COM_reg, 0) == true) {
+		stepfase++;
+		if (stepfase > 7)stepfase = 0;
+	}
+	else {
+		stepfase--;
+		if (stepfase > 7)stepfase = 7;
+	}
 }
-
-
-void dataout() {
+void switchset() { //shifts the switch puls
 	switchcount++;
 	if (switchcount > 2)switchcount = 0;
-	byte temp = shiftbyte;
-
 	switch (switchcount) {
 	case 0:
-		temp = 6; //00000110
+		shiftbyte |= (6 << 0);
+		shiftbyte &= ~(1 << 0);
 		break;
 	case 1:
-		temp = 5; //000000101
+		shiftbyte |= (5 << 0);
+		shiftbyte &= ~(2 << 0);
 		break;
 	case 2:
-		temp = 3; //000000011
+		shiftbyte |= (3 << 0);
+		shiftbyte &= ~(4 << 0);
 		break;
 	}
-	shiftbyte = shiftbyte >> 3;
-	shiftbyte = shiftbyte << 3;
-	shiftbyte = shiftbyte + temp;
 }
 
 void SHIFT() {
@@ -388,40 +408,45 @@ void SHIFT() {
 	bit 0-2 via 3 schakelaars terugleiden naar portB4 deze als input, na shift port b4 lezen overeenkomend met welke uitgang
 	van de shiftregister dan laag is. 3 schakelaars drukknoppen dus....
 
-
 	*/
 
 	//eerst
-
-	boolean status;
-	dataout();
-
+	switchset();
 	Shift1();
 	//latch shift register
 	PINB |= (1 << 3);
 	PINB |= (1 << 3);
 
-	//overig
-		//now check switches?
+	switches();
+}
+void switches() {
+	boolean status;
 	status = bitRead(PINB, 4);
 	if (status != bitRead(switchstatus, switchcount)) {
 
 		if (status == false) { //switch pushed
 			switchstatus &= ~(1 << switchcount);
+			//shiftbyte ^= (1 << switchcount + 5);
 
-			//payload...
-			shiftbyte ^= (1 << switchcount + 5);
-			//if(switchcount==0) COM_reg ^= (1 << 0);
+			switch (switchcount) {
+			case 0:
+				COM_reg ^= (1 << 0); //direction change
+				break;
+			case 1:
+				shiftbyte &= ~(15 << 4);
+				COM_reg ^= (1 << 1);
+				break;
+			case 2:
+				break;
+			}
 		}
 		else { //switch released
+			//shiftbyte &= ~(1 << switchcount +5);
 			switchstatus |= (1 << switchcount);
 		}
 	}
 	else { //dus geen verandering en status is false dus knop ingedrukt, hier de knopvasthoud programmering realiseren.
-
 	}
-
-
 }
 void Shift1() {
 	//shift 1e byte
@@ -436,16 +461,10 @@ void Shift1() {
 void loop() {
 	shiftcount++;
 	DEK_DCCh();
-	if (shiftcount==0) SHIFT();
-
-
-	/*
-	test om verstreken tijd naar werkelijke tijd te bepalen
+	if (shiftcount == 0) SHIFT();
 	if (millis() - Ctime > 1) {
 		Ctime = millis();
 		//dataout();
-		//STEP();
-			}
-*/
-
+		if (bitRead(COM_reg,1)==true) steps();
+	}
 }
