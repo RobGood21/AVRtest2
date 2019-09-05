d0290a259a7f98b25b75d6750e82e7ebb137a7fe
Rob Antonisse <robantonisse@gmail.com>
2019-09-05 22:03:41 +0200

    Dutchie

---
+++ DutChie.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		DutChie.ino
+ Created:	9/5/2019 9:42:29 PM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	//dit is een tekst
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
